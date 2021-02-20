ab9c1d88bcb97b9c9a6e37c0f4ffc7dad411edf5
kokemaru2427 <kai0405kai0407@icloud.com>
2021-02-20 19:33:56 +0900

    ユーザー登録機能実装
    新規登録、ログイン、ログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :config_permiyyed_parameters, if: :devise_controller?
+
+  private
+  
+  def config_permiyyed_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :school_year])
+  end
 end
