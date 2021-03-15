75bd39226554d97d7458169cc99b0e5f88b081e6
kokemaru2427 <kai0405kai0407@icloud.com>
2021-03-15 13:22:06 +0900

    repo_urlの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'FREE-O'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:kokemaru2427/FREE-O-app.git'
+set :repo_url,  'git@github.com:kokemaru2427/FREE-O.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
